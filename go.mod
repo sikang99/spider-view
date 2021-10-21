d7f298ed2696b154f0c4d63bb5fbc8003c468b2b
stoney <sikang99@gmail.com>
2021-10-21 20:22:26 +0900

    0.0.0.1,stoney - enhancing

--- go.mod
+++ go.mod
@@ -3,33 +3,35 @@ module github.com/sikang99/spider-view
 go 1.17
 
 require (
-	github.com/pion/example-webrtc-applications/v3 v3.0.4
-	github.com/pion/rtcp v1.2.8
-	github.com/pion/webrtc/v3 v3.1.5
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/pion/webrtc/v2 v2.2.26 // indirect
 	gocv.io/x/gocv v0.28.0
 )
 
 require (
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/cheekybits/genny v1.0.0 // indirect
+	github.com/google/uuid v1.1.1 // indirect
+	github.com/lucas-clemente/quic-go v0.7.1-0.20190401152353-907071221cf9 // indirect
+	github.com/marten-seemann/qtls v0.2.3 // indirect
 	github.com/pion/datachannel v1.4.21 // indirect
-	github.com/pion/dtls/v2 v2.0.10 // indirect
-	github.com/pion/ice/v2 v2.1.12 // indirect
-	github.com/pion/interceptor v0.1.0 // indirect
+	github.com/pion/dtls/v2 v2.0.2 // indirect
+	github.com/pion/ice v0.7.18 // indirect
 	github.com/pion/logging v0.2.2 // indirect
-	github.com/pion/mdns v0.0.5 // indirect
+	github.com/pion/mdns v0.0.4 // indirect
+	github.com/pion/quic v0.1.1 // indirect
 	github.com/pion/randutil v0.1.0 // indirect
-	github.com/pion/rtp v1.7.2 // indirect
-	github.com/pion/sctp v1.7.12 // indirect
-	github.com/pion/sdp/v3 v3.0.4 // indirect
-	github.com/pion/srtp/v2 v2.0.5 // indirect
+	github.com/pion/rtcp v1.2.8
+	github.com/pion/rtp v1.6.0 // indirect
+	github.com/pion/sctp v1.7.10 // indirect
+	github.com/pion/sdp/v2 v2.4.0 // indirect
+	github.com/pion/srtp v1.5.1 // indirect
 	github.com/pion/stun v0.3.5 // indirect
-	github.com/pion/transport v0.12.3 // indirect
-	github.com/pion/turn/v2 v2.0.5 // indirect
-	github.com/pion/udp v0.1.1 // indirect
+	github.com/pion/transport v0.10.1 // indirect
+	github.com/pion/turn/v2 v2.0.4 // indirect
+	github.com/pion/udp v0.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
-	golang.org/x/net v0.0.0-20211005001312-d4b1ae081e3b // indirect
-	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899 // indirect
+	golang.org/x/net v0.0.0-20200707034311-ab3426394381 // indirect
+	golang.org/x/sys v0.0.0-20200724161237-0e2f3a69832c // indirect
+	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
 )
--- main.go
+++ main.go
@@ -12,15 +12,20 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"strconv"
+	"time"
 
 	"github.com/gorilla/websocket"
+	"gocv.io/x/gocv"
 
-	"github.com/pion/webrtc/v3"
+	"github.com/pion/rtcp"
+	"github.com/pion/webrtc/v2"
+	"github.com/pion/webrtc/v2/pkg/media/ivfwriter"
 )
 
 //---------------------------------------------------------------------------------
@@ -29,24 +34,34 @@ type WsMessage struct {
 	Data string `json:"data"`
 }
 
-type PGConfig struct {
-	VideoWidth       int       `json:"video_width,omitempty"`
-	VideoHeight      int       `json:"video_height,omitempty"`
-	BitRate          int       `json:"bit_rate,omitempty"`
-	KeyFrameInterval int       `json:"key_frame_interval,omitempty"`
-	VideoNouse       bool      `json:"video_nouse,omitempty"`
-	AudioNouse       bool      `json:"audio_nouse,omitempty"`
-	VideoCodec       string    `json:"video_codec,omitempty"`
-	AudioCodec       string    `json:"audio_codec,omitempty"`
-	VideoType        string    `json:"video_type,omitempty"`
-	AudioType        string    `json:"audio_type,omitempty"`
-	VideoLabel       string    `json:"video_label,omitempty"` // video device name (label)
-	AudioLabel       string    `json:"audio_label,omitempty"` // audio device name (label)
-	ICEServer        string    `json:"ice_server,omitempty"`
-	SpiderServer     string    `json:"spider_server,omitempty"`
-	ChannelID        string    `json:"channel_id,omitempty"`
-	URL              string    `json:"url,omitempty"`
-	ffmpeg           *exec.Cmd `json:"-"`
+type Program struct {
+	Ok               bool   `json:"ok"`
+	VideoWidth       int    `json:"video_width,omitempty"`
+	VideoHeight      int    `json:"video_height,omitempty"`
+	BitRate          int    `json:"bit_rate,omitempty"`
+	KeyFrameInterval int    `json:"key_frame_interval,omitempty"`
+	VideoNouse       bool   `json:"video_nouse,omitempty"`
+	AudioNouse       bool   `json:"audio_nouse,omitempty"`
+	VideoCodec       string `json:"video_codec,omitempty"`
+	AudioCodec       string `json:"audio_codec,omitempty"`
+	VideoType        string `json:"video_type,omitempty"`
+	AudioType        string `json:"audio_type,omitempty"`
+	VideoLabel       string `json:"video_label,omitempty"` // video device name (label)
+	AudioLabel       string `json:"audio_label,omitempty"` // audio device name (label)
+	ICEServer        string `json:"ice_server,omitempty"`
+	SpiderServer     string `json:"spider_server,omitempty"`
+	ChannelID        string `json:"channel_id,omitempty"`
+	URL              string `json:"url,omitempty"`
+	// -- Internal handling parts
+	pc     *webrtc.PeerConnection
+	ws     *websocket.Conn
+	msgch  chan WsMessage
+	ffmpeg struct {
+		cmd    *exec.Cmd
+		stdin  io.WriteCloser
+		stdout io.ReadCloser
+		stderr io.ReadCloser
+	} `json:"-"`
 }
 
 //---------------------------------------------------------------------------------
@@ -59,7 +74,8 @@ func main() {
 	fmt.Printf("Spider Video Viewer, v%s (c)TeamGRIT, 2021\n", Version)
 
 	// Default program settings
-	pgConfig := &PGConfig{
+	pg := &Program{
+		Ok:               true,
 		VideoWidth:       1280,
 		VideoHeight:      720,
 		BitRate:          1_000_000, // 1Mbps
@@ -73,39 +89,41 @@ func main() {
 		ICEServer:        "cobot.center:3478",
 		SpiderServer:     "localhost:8267",
 		ChannelID:        "bq5ame6g10l3jia3h0ng", // CoJam.Shop channel
+		msgch:            make(chan WsMessage, 2),
 	}
 
 	// Handle command line options
-	// flag.IntVar(&pgConfig.VideoWidth, "vwidth", pgConfig.VideoWidth, "video width to use")
-	// flag.IntVar(&pgConfig.VideoHeight, "vheight", pgConfig.VideoHeight, "video height to use")
-	// flag.BoolVar(&pgConfig.VideoNouse, "vnouse", pgConfig.VideoNouse, "video no use")
-	// flag.BoolVar(&pgConfig.AudioNouse, "anouse", pgConfig.AudioNouse, "audio no use")
-	// flag.StringVar(&pgConfig.VideoType, "vtype", pgConfig.VideoType, "video device type [camera|screen]")
-	// flag.StringVar(&pgConfig.AudioType, "atype", pgConfig.AudioType, "audio device type [microphone]")
-	// flag.StringVar(&pgConfig.VideoLabel, "vlabel", pgConfig.VideoLabel, "label of video device to select")
-	// flag.StringVar(&pgConfig.AudioLabel, "alabel", pgConfig.AudioLabel, "label of audio device to select")
-	// flag.StringVar(&pgConfig.VideoCodec, "vcodec", pgConfig.VideoCodec, "video codec to use [h264|vp8|vp9]")
-	// flag.StringVar(&pgConfig.AudioCodec, "acodec", pgConfig.AudioCodec, "audio codec to use [opus]")
-	flag.IntVar(&pgConfig.BitRate, "brate", pgConfig.BitRate, "bit rate of video to send in bps")
-	flag.IntVar(&pgConfig.KeyFrameInterval, "kint", pgConfig.KeyFrameInterval, "key frame interval")
-	flag.StringVar(&pgConfig.ICEServer, "ice", pgConfig.ICEServer, "ice server address to use")
-	flag.StringVar(&pgConfig.SpiderServer, "spider", pgConfig.SpiderServer, "ice server address to use")
-	flag.StringVar(&pgConfig.ChannelID, "channel", pgConfig.ChannelID, "channel id of spider server to send")
-	flag.StringVar(&pgConfig.URL, "url", pgConfig.URL, "url of spider server to connect")
+	// flag.IntVar(&pg.VideoWidth, "vwidth", pg.VideoWidth, "video width to use")
+	// flag.IntVar(&pg.VideoHeight, "vheight", pg.VideoHeight, "video height to use")
+	// flag.BoolVar(&pg.VideoNouse, "vnouse", pg.VideoNouse, "video no use")
+	// flag.BoolVar(&pg.AudioNouse, "anouse", pg.AudioNouse, "audio no use")
+	// flag.StringVar(&pg.VideoType, "vtype", pg.VideoType, "video device type [camera|screen]")
+	// flag.StringVar(&pg.AudioType, "atype", pg.AudioType, "audio device type [microphone]")
+	// flag.StringVar(&pg.VideoLabel, "vlabel", pg.VideoLabel, "label of video device to select")
+	// flag.StringVar(&pg.AudioLabel, "alabel", pg.AudioLabel, "label of audio device to select")
+	// flag.StringVar(&pg.VideoCodec, "vcodec", pg.VideoCodec, "video codec to use [h264|vp8|vp9]")
+	// flag.StringVar(&pg.AudioCodec, "acodec", pg.AudioCodec, "audio codec to use [opus]")
+	flag.IntVar(&pg.BitRate, "brate", pg.BitRate, "bit rate of video to send in bps")
+	flag.IntVar(&pg.KeyFrameInterval, "kint", pg.KeyFrameInterval, "key frame interval")
+	flag.StringVar(&pg.ICEServer, "ice", pg.ICEServer, "ice server address to use")
+	flag.StringVar(&pg.SpiderServer, "spider", pg.SpiderServer, "ice server address to use")
+	flag.StringVar(&pg.ChannelID, "channel", pg.ChannelID, "channel id of spider server to send")
+	flag.StringVar(&pg.URL, "url", pg.URL, "url of spider server to connect")
 	flag.Parse()
 
-	if pgConfig.URL == "" {
-		pgConfig.URL = fmt.Sprintf("wss://%s/live/ws/sub?channel=%s&vcodec=%s",
-			pgConfig.SpiderServer, pgConfig.ChannelID, pgConfig.VideoCodec)
+	if pg.URL == "" {
+		pg.URL = fmt.Sprintf("wss://%s/live/ws/sub?channel=%s&vcodec=%s",
+			pg.SpiderServer, pg.ChannelID, pg.VideoCodec)
 	}
 
-	err := pgConfig.prepareFFmpeg()
+	err := pg.openFFmpeg()
 	if err != nil {
 		log.Println(err)
 		return
 	}
+	defer pg.closeFFmpeg()
 
-	ws, err := ConnectWebsocketByUrl(pgConfig.URL, 1024)
+	ws, err := pg.connectWebsocketByUrl(pg.URL, 1024)
 	if err != nil {
 		log.Println(err)
 		return
@@ -113,21 +131,28 @@ func main() {
 	defer ws.Close()
 
 	// -- webrtc configuration to use
-	rtcConfig := pgConfig.SetRTCConfiguratrion()
+	rtcConfig := pg.setRTCConfiguratrion()
 	// rc, _ := json.Marshal(rtcConfig.SDPSemantics)
 	// rc, _ := json.Marshal(rtcConfig)
 	// log.Println(string(rc))
 
-	mediaEngine := webrtc.MediaEngine{}
-	// log.Println(mediaEngine)
+	me := webrtc.MediaEngine{}
+	me.RegisterDefaultCodecs()
 
-	api := webrtc.NewAPI(webrtc.WithMediaEngine(&mediaEngine))
+	api := webrtc.NewAPI(webrtc.WithMediaEngine(me))
 	pc, err := api.NewPeerConnection(rtcConfig)
+
 	if err != nil {
 		log.Println(err)
 		return
 	}
 	// log.Println(pc)
+
+	ivfWriter, err := ivfwriter.NewWith(pg.ffmpeg.stdin)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
 	pc.OnICEConnectionStateChange(func(connectionState webrtc.ICEConnectionState) {
 		log.Println("w.OnICEConnectionState:", connectionState)
@@ -139,61 +164,95 @@ func main() {
 	pc.OnICECandidate(func(iceCandidate *webrtc.ICECandidate) {
 		log.Println("w.OnICECandidate:", iceCandidate)
 		// send candidates to the peer (server)
-		if iceCandidate != nil {
-			candidate := iceCandidate.ToJSON()
-			data, err := json.Marshal(candidate)
+		// if iceCandidate != nil {
+		// 	candidate := iceCandidate.ToJSON()
+		// 	data, err := json.Marshal(candidate)
+		// 	if err != nil {
+		// 		log.Println("json.Marshal", err)
+		// 		return
+		// 	}
+		// 	pg.msgch <- WsMessage{
+		// 		Type: "send-candidate2",
+		// 		Data: string(data),
+		// 	}
+		// }
+	})
+
+	pc.OnTrack(func(track *webrtc.Track, receiver *webrtc.RTPReceiver) {
+		// Send a PLI on an interval so that the publisher is pushing a keyframe every rtcpPLIInterval
+		go func() {
+			ticker := time.NewTicker(time.Second * 3)
+			for range ticker.C {
+				err := pc.WriteRTCP([]rtcp.Packet{&rtcp.PictureLossIndication{MediaSSRC: uint32(track.SSRC())}})
+				if err != nil {
+					log.Println(err)
+					return
+				}
+			}
+		}()
+
+		log.Printf("Track has started, of type %d: %s \n",
+			track.PayloadType(), track.Codec().RTPCodecCapability.MimeType)
+		for {
+			// Read RTP packets being sent to Pion
+			rtp, err := track.ReadRTP()
 			if err != nil {
-				log.Println("json.Marshal", err)
+				log.Println(err)
 				return
 			}
-			err = ws.WriteJSON(&WsMessage{
-				Type: "candidate2",
-				Data: string(data),
-			})
+
+			err = ivfWriter.WriteRTP(rtp)
 			if err != nil {
-				log.Println("json.Marshal", err)
+				log.Println(err)
 				return
 			}
 		}
 	})
 
-	err = SendOfferByWebsocket(ws, pc)
+	// go pg.detectMotion()
+
+	err = pg.sendOfferByWebsocket(ws, pc)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	err = ProcMessageByWebsocket(ws, pc)
+	err = pg.procMessageByWebsocket(ws, pc)
 	if err != nil {
 		log.Println(err)
 		return
 	}
+
 }
 
 //---------------------------------------------------------------------------------
-func (d *PGConfig) prepareFFmpeg() (err error) {
-	d.ffmpeg = exec.Command("ffmpeg", "-i", "pipe:0", "-pix_fmt", "bgr24", "-s",
+func (d *Program) openFFmpeg() (err error) {
+	log.Println("i.openFFmpeg:", "bgr24")
+
+	d.ffmpeg.cmd = exec.Command("ffmpeg", "-i", "pipe:0", "-pix_fmt", "bgr24", "-s",
 		strconv.Itoa(d.VideoWidth)+"x"+strconv.Itoa(d.VideoHeight), "-f", "rawvideo", "pipe:1") //nolint
-	if d.ffmpeg == nil {
+	if d.ffmpeg.cmd == nil {
 		err = fmt.Errorf("ffmpeg exec error")
 		log.Println(err)
 		return
 	}
 
-	ffmpegErr, err := d.ffmpeg.StderrPipe()
+	d.ffmpeg.stdin, _ = d.ffmpeg.cmd.StdinPipe()
+	d.ffmpeg.stdout, _ = d.ffmpeg.cmd.StdoutPipe()
+	d.ffmpeg.stderr, err = d.ffmpeg.cmd.StderrPipe()
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	err = d.ffmpeg.Start()
+	err = d.ffmpeg.cmd.Start()
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
 	go func() {
-		scanner := bufio.NewScanner(ffmpegErr)
+		scanner := bufio.NewScanner(d.ffmpeg.stderr)
 		for scanner.Scan() {
 			log.Println(scanner.Text())
 		}
@@ -201,9 +260,45 @@ func (d *PGConfig) prepareFFmpeg() (err error) {
 	return
 }
 
+func (d *Program) closeFFmpeg() (err error) {
+	d.ffmpeg.cmd = nil
+	return
+}
+
 //---------------------------------------------------------------------------------
-func (d *PGConfig) SetRTCConfiguratrion() (rtcConfig webrtc.Configuration) {
-	log.Println("i.SetRTCConfiguratrion:", d.ICEServer)
+func (d *Program) detectMotion() (err error) {
+	log.Println("i.detectMotion")
+
+	window := gocv.NewWindow("Spider Video Viewer")
+	defer window.Close() //nolint
+
+	img := gocv.NewMat()
+	defer img.Close() //nolint
+
+	for d.Ok {
+		buf := make([]byte, d.VideoWidth*d.VideoHeight*3)
+		_, err := io.ReadFull(d.ffmpeg.stdout, buf)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		img, _ := gocv.NewMatFromBytes(d.VideoHeight, d.VideoWidth, gocv.MatTypeCV8UC3, buf)
+		if img.Empty() {
+			continue
+		}
+
+		window.IMShow(img)
+		if window.WaitKey(1) == 27 {
+			break
+		}
+	}
+
+	return
+}
+
+//---------------------------------------------------------------------------------
+func (d *Program) setRTCConfiguratrion() (rtcConfig webrtc.Configuration) {
+	log.Println("i.setRTCConfiguratrion:", d.ICEServer)
 
 	rtcConfig = webrtc.Configuration{
 		ICEServers: []webrtc.ICEServer{
@@ -228,8 +323,8 @@ func (d *PGConfig) SetRTCConfiguratrion() (rtcConfig webrtc.Configuration) {
 }
 
 //---------------------------------------------------------------------------------
-func ConnectWebsocketByUrl(url string, bsize int) (ws *websocket.Conn, err error) {
-	log.Println("i.ConnectUrlByWebsocket:", url)
+func (d *Program) connectWebsocketByUrl(url string, bsize int) (ws *websocket.Conn, err error) {
+	log.Println("i.connectUrlByWebsocket:", url)
 
 	var dialer = websocket.Dialer{
 		ReadBufferSize:  bsize,
@@ -243,12 +338,15 @@ func ConnectWebsocketByUrl(url string, bsize int) (ws *websocket.Conn, err error
 		log.Println(err)
 		return
 	}
+
+	d.ws = ws
 	return
 }
 
 //---------------------------------------------------------------------------------
-func SendOfferByWebsocket(ws *websocket.Conn, pc *webrtc.PeerConnection) (err error) {
-	defer log.Println("o.SendOfferByWebsocket", err)
+func (d *Program) sendOfferByWebsocket(ws *websocket.Conn, pc *webrtc.PeerConnection) (err error) {
+	log.Println("i.sendOfferByWebsocket")
+	defer log.Println("o.sendOfferByWebsocket", err)
 
 	offer, err := pc.CreateOffer(nil)
 	if err != nil {
@@ -263,94 +361,110 @@ func SendOfferByWebsocket(ws *websocket.Conn, pc *webrtc.PeerConnection) (err er
 		return
 	}
 
-	err = ws.WriteJSON(&WsMessage{
-		Type: "offer",
+	d.msgch <- WsMessage{
+		Type: "send-offer",
 		Data: offer.SDP,
-	})
-	if err != nil {
-		log.Println(err)
-		return
 	}
-
 	return
 }
 
 //---------------------------------------------------------------------------------
-func ProcMessageByWebsocket(ws *websocket.Conn, pc *webrtc.PeerConnection) (err error) {
+func (d *Program) procMessageByWebsocket(ws *websocket.Conn, pc *webrtc.PeerConnection) (err error) {
+	log.Println("i.procMessageByWebsocket")
 	defer log.Println("o.ProcMessageByWebsocket", err)
 
-	for {
-		m := WsMessage{}
-		err = ws.ReadJSON(&m)
-		if err != nil {
-			log.Println(err)
-			return
-		}
+	ticker := time.NewTicker(30 * time.Second)
+	defer ticker.Stop()
 
-		switch m.Type {
-		case "ping", "joins":
-			log.Println("[msg]", m)
-		case "offer":
-			offer := webrtc.SessionDescription{
-				Type: webrtc.SDPTypeOffer,
-				SDP:  m.Data,
-			}
-			err = pc.SetRemoteDescription(offer)
-			if err != nil {
-				log.Println("pc.SetRemoteDescription", err)
-				return
-			}
-			answer, err := pc.CreateAnswer(nil)
-			if err != nil {
-				log.Println(err)
-				return err
-			}
-			err = pc.SetLocalDescription(answer)
-			if err != nil {
-				log.Println("pc.SetLocalDescription", err)
-				return err
-			}
+	for d.Ok {
+		select {
+		case <-ticker.C:
 			ws.WriteJSON(&WsMessage{
-				Type: "answer",
-				Data: answer.SDP,
+				Type: "ping",
 			})
-		case "answer":
-			answer := webrtc.SessionDescription{
-				Type: webrtc.SDPTypeAnswer,
-				SDP:  m.Data,
-			}
-			err = pc.SetRemoteDescription(answer)
-			if err != nil {
-				log.Println("pc.SetRemoteDescription", err)
+		case m, ok := <-d.msgch:
+			if !ok {
+				err = fmt.Errorf("msgch recv error")
+				log.Println(err)
 				return
 			}
-		case "candidate": // old style, pion/mediadevices
-			// log.Println("msg.data", m.Data)
-			candidate := webrtc.ICECandidateInit{
-				Candidate: m.Data,
+			// log.Println(m.Type)
+			switch m.Type {
+			case "ping", "joins":
+				log.Println("[msg]", m)
+			case "offer":
+				offer := webrtc.SessionDescription{
+					Type: webrtc.SDPTypeOffer,
+					SDP:  m.Data,
+				}
+				err = pc.SetRemoteDescription(offer)
+				if err != nil {
+					log.Println("pc.SetRemoteDescription", err)
+					return
+				}
+				answer, err := pc.CreateAnswer(nil)
+				if err != nil {
+					log.Println(err)
+					return err
+				}
+				err = pc.SetLocalDescription(answer)
+				if err != nil {
+					log.Println("pc.SetLocalDescription", err)
+					return err
+				}
+				ws.WriteJSON(&WsMessage{
+					Type: "answer",
+					Data: answer.SDP,
+				})
+			case "answer":
+				answer := webrtc.SessionDescription{
+					Type: webrtc.SDPTypeAnswer,
+					SDP:  m.Data,
+				}
+				err = pc.SetRemoteDescription(answer)
+				if err != nil {
+					log.Println("pc.SetRemoteDescription", err)
+					return
+				}
+			case "candidate": // old style, pion/mediadevices
+				// log.Println("msg.data", m.Data)
+				candidate := webrtc.ICECandidateInit{
+					Candidate: m.Data,
+				}
+				err = pc.AddICECandidate(candidate)
+				if err != nil {
+					log.Println("pc.AddICECandidate:", err)
+					return
+				}
+			case "candidate2": // new style, standard type
+				// log.Println("msg.data", m.Data)
+				candidate := webrtc.ICECandidateInit{}
+				err = json.Unmarshal([]byte(m.Data), &candidate)
+				if err != nil {
+					log.Println("json.Unmarshal:", err)
+					break
+				}
+				err = pc.AddICECandidate(candidate)
+				if err != nil {
+					log.Println("pc.AddICECandidate:", err)
+					return
+				}
+			case "send-offer":
+				ws.WriteJSON(&WsMessage{
+					Type: "offer",
+					Data: m.Data,
+				})
+			case "send-candidate2":
+				ws.WriteJSON(&WsMessage{
+					Type: "candidate2",
+					Data: m.Data,
+				})
+			default:
+				log.Println("unknown [msg]", m)
 			}
-			err = pc.AddICECandidate(candidate)
-			if err != nil {
-				log.Println("pc.AddICECandidate:", err)
-				return
-			}
-		case "candidate2": // new style, standard type
-			// log.Println("msg.data", m.Data)
-			candidate := webrtc.ICECandidateInit{}
-			err = json.Unmarshal([]byte(m.Data), &candidate)
-			if err != nil {
-				log.Println("json.Unmarshal:", err)
-				break
-			}
-			err = pc.AddICECandidate(candidate)
-			if err != nil {
-				log.Println("pc.AddICECandidate:", err)
-				return
-			}
-		default:
-			log.Println("unknown [msg]", m)
 		}
 	}
+	return
 }
 
 //=================================================================================
